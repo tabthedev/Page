f9a897108dae2587065d60c5042ebb2e0cbfbd14
탭・TAB・タブ <me@tabthedev.kr>
2026-02-14 22:13:50 +0900

    Add router (experimental)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import { useState } from 'react';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function Home() {
+  return <>
+    <h1>넣을게 없다</h1>
+    <br />
+    <Link to="birthday-2025">2025 생일</Link>
+    <br />
+    <Link to="birthday-2026">2026 생일</Link>
+  </>
+}
+
+function bb() {
+  return <>
+    <h1>앙</h1>
+  </>
+}
+
+
 
 function App() {
   return (
     <>
-        <h1>넣을게 없다</h1>
-        <a href='birthday-2025'>2025 생일🎂</a>
-        <a href='birthday-2026'>2026 생일🎂</a>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/birthday-2025" element={<bb/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
