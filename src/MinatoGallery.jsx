036ee251ae37b7b38dd47d019e377f1945461498
탭・TAB・タブ <me@tabthedev.kr>
2026-03-23 03:41:02 +0900

    fix image overflow & sharp corner

--- src/MinatoGallery.jsx
+++ src/MinatoGallery.jsx
@@ -28,8 +28,8 @@ function MinatoGallery() {
         
       }}>
       <img src={imagePath} style={{
-        width: "100%",
-        height: "100%",
+        maxHeight: "100%",
+        maxWidth: "100%",
         "objectFit": "contain",
         "cursor": "pointer",
         "borderRadius": "10px",
