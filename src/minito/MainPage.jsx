6bf8a41066ca9b5a85675795cc041c7e96e1f0bd
탭・TAB・タブ <me@tabthedev.kr>
2026-04-08 21:52:58 +0900

    리디렉션 페이지

---
+++ src/minito/MainPage.jsx
@@ -0,0 +1,5 @@
+export function MinitoBot_MainPage() {
+    return <>
+        <meta http-equiv="refresh" content="0; url=https://discord.com/oauth2/authorize?client_id=1488118588014592150" />
+    </>
+}
