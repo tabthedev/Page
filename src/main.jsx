d1d8100749be74a4c029fe242b4efefcc4de77ec
탭・TAB・タブ <me@tabthedev.kr>
2026-04-08 21:58:51 +0900

    초기화 오류 수정

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import App from './App.jsx'
+import App from './pages/Main.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
