7560b797eb5a4a46c3fcdf767d67c0658981cf27
탭・TAB・タブ <me@tabthedev.kr>
2026-04-08 21:57:40 +0900

    소스 트리 구조 변경

---
+++ src/pages/minato/minito/PrivacyPolicy.jsx
@@ -0,0 +1,49 @@
+import { StickyHeader } from '../../utils/StickyHeader'
+import './style.css'
+
+export function MinitoBot_PrivacyPolicy() {
+    return <div className='minitoBotInfos'>
+        {StickyHeader(<h1 style={{textAlign: "center", justifyItems: "center"}}>Privacy Policy for "Minito-bot"</h1>)}
+        <div style={{height:"20px"}}></div>
+
+        This Privacy Policy governs the collection, use, and sharing of personal information by <b>Minito-bot</b>, a Discord bot developed by <b>@tabthedev</b>. <br />
+        By using <b>Minito-bot</b>, you agree to the terms of this Privacy Policy. <br />
+
+        <h2>Information We Collect</h2>
+
+        We collect information that you provide to us through your use of the bot, such as your Discord user ID and username, server and channel information, and message content. <br />
+        We may also collect usage data, such as the frequency and duration of your use of the bot. <br />
+        <u>In particular, We may track your interactions with the bot, including commands used and responses received, to improve our services and provide a better user experience.</u>
+
+        <h2>How We Use Your Information</h2>
+
+        We use your information to operate and improve <b>Minito-bot</b>, including to provide support and respond to your requests. <br />
+        We may also use your information to develop new features or services, to conduct research and analytics, and to comply with legal obligations.
+
+        <h2>Sharing Your Information</h2>
+
+        We do not sell or share your personal information with third parties. <br />
+        However, we may disclose your information in response to legal process or a request from a law enforcement agency or regulatory authority.
+
+        <h2>Data Retention</h2>
+
+        We retain your information for as long as necessary to provide <b>Minito-bot</b>’s services or as required by law. <br />
+        We will delete your information upon your request or when it is no longer needed.
+
+        <h2>Data Security</h2>
+
+        We take reasonable measures to protect your information from unauthorized access, alteration, or destruction. However, no security measure is perfect, and we cannot guarantee the security of your information.
+
+        <h2>Changes to this Policy</h2>
+
+        We may update this Privacy Policy from time to time, and we will post the updated policy on our website. Your continued use of <b>Minito-bot</b> after we make changes to this policy indicates your acceptance of the revised policy.
+
+        <h2>Contact Us</h2>
+
+        If you have any questions or concerns about this Privacy Policy, please contact us at <b>me@tabthedev.kr</b>.
+
+        <h2>Effective Date</h2>
+
+        This Privacy Policy is effective as this bot gets removed.
+    </div>
+}
