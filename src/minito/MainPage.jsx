9312e5cd9ad64fee97ddd504ab7c90cf97e285e1
탭・TAB・タブ <me@tabthedev.kr>
2026-04-08 21:56:19 +0900

    소스 경로 구조 수정

--- src/minito/MainPage.jsx
+++
@@ -1,5 +0,0 @@
-export function MinitoBot_MainPage() {
-    return <>
-        <meta http-equiv="refresh" content="0; url=https://discord.com/oauth2/authorize?client_id=1488118588014592150" />
-    </>
-}
