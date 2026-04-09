d899450eeb9ce59a4a62c8dbcbe36a6ee8540d80
탭・TAB・タブ <me@tabthedev.kr>
2026-04-09 13:35:55 +0900

    Redirect 모듈화

--- src/pages/minato/minitoBot/MainPage.jsx
+++ src/pages/minato/minitoBot/MainPage.jsx
@@ -1,5 +1,5 @@
+import Redirect from "../../../utils/Redirect";
+
 export function MinitoBot_MainPage() {
-    return <>
-        <meta http-equiv="refresh" content="0; url=https://discord.com/oauth2/authorize?client_id=1488118588014592150" />
-    </>
+    return Redirect("https://discord.com/oauth2/authorize?client_id=1488118588014592150")
 }
---
+++ src/utils/Redirect.jsx
@@ -0,0 +1,5 @@
+export default function(url) {
+    return <>
+        <meta http-equiv="refresh" content={"0; url="+url} />
+    </>
+}
