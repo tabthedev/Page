0c954ea3249b4db7eb54c236d0b88464040c70a7
탭・TAB・タブ <me@tabthedev.kr>
2026-07-20 22:00:57 +0900

    utils->template으로 명칭변경

--- src/pages/minato/minitoBot/TermsOfService.jsx
+++ src/pages/minato/minitoBot/TermsOfService.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import StickyHeader from '../../../utils/StickyHeader.jsx';
+import StickyHeader from '../../../templates/StickyHeader.jsx';
 import './style.css'
 
 export function MinitoBot_TermsOfService() {
